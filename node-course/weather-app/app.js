9d5dfce988d9fc3933d75df66d7c4a3956a672c8
lelacgozarin05 <55863369+lelacgozarin05@users.noreply.github.com>
2021-10-17 22:24:37 +0800

    node_modules

--- node-course/weather-app/app.js
+++ node-course/weather-app/app.js
@@ -3,5 +3,7 @@ const request = require('request')
 const url = 'http://api.weatherstack.com/current?access_key=b228ea66a5dfd37a18a7ecf6c83e61e6&query=37.8267,-122.4233'
 
 request({ url: url }, (error, response) => {
-    console.log(response)
+    const data = JSON.parse(response.body)
+    console.log(data.current)
+
 })
