8b1519d735606cc8bbfa98d5aa12c906a010cb99
lelacgozarin05 <55863369+lelacgozarin05@users.noreply.github.com>
2021-10-18 15:31:34 +0800

    change branch

--- node-course/weather-app/app.js
+++ node-course/weather-app/app.js
@@ -1,8 +1,10 @@
 const request = require('request')
 
-const url = 'http://api.weatherstack.com/current?access_key=b228ea66a5dfd37a18a7ecf6c83e61e6&query=37.8267,-122.4233'
+const url = 'http://api.weatherstack.com/current?access_key=b228ea66a5dfd37a18a7ecf6c83e61e6&query=37.8267,-122.4233&units=f'
 
-request({ url: url }, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.current)
+request({ url: url, json: true }, (error, response) => {
+//    console.log(response.body.current)
+
+    console.log(response.body.current.weather_descriptions[0] + ". It is currently " + response.body.current.temperature + 'degress out. There is a ' + response.body.current.precip + '% chance of Rain')
 })
+//Geocoding
